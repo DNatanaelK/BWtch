19ab694706c7c8b963a0fdb56e53bea7736ba10f
DNatanaelK <43308843+DNatanaelK@users.noreply.github.com>
2018-10-08 16:06:58 +0800

    action_delete_all_transactions button menu

--- app/src/main/java/protect/budgetwatch/TransactionActivity.java
+++ app/src/main/java/protect/budgetwatch/TransactionActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
@@ -20,10 +21,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
 
+
+
 public class TransactionActivity extends AppCompatActivity
 {
     private TransactionDatabaseChangedReceiver _dbChanged;
     private static final String TAG = "BudgetWatch";
+
+    private DBHelper _db;
+    private int _transactionId;
 
     private boolean _currentlySearching = false;
 
@@ -105,6 +111,7 @@ public class TransactionActivity extends AppCompatActivity
         }
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -115,6 +122,8 @@ public class TransactionActivity extends AppCompatActivity
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        _db = new DBHelper(this);
 
         searchView.setOnCloseListener(new SearchView.OnCloseListener()
         {
@@ -193,6 +202,10 @@ public class TransactionActivity extends AppCompatActivity
             builder.show();
             return true;
         }
+        if(id == R.id.action_delete_all_transactions){
+            _db.deleteAllTransactions();
+        }
+
 
         if(id == android.R.id.home)
         {
